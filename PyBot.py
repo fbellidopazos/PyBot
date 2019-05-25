4c7e4e363719fba7d520b8c59abe2d1cc5d2b524
Fernando Bellido <fbellidopazos@gmail.com>
2019-05-25 21:43:37 +0200

    Pybot

--- PyBot.py
+++ PyBot.py
@@ -1,20 +1,19 @@
-##Prepare https://www.devdungeon.com/content/make-discord-bot-python
-##=====================================================
+# Prepare https://www.devdungeon.com/content/make-discord-bot-python
+# =====================================================
 import random
 import asyncio
-import aiohttp
-import json
+# import aiohttp
+# import json
 from discord import Game
 from discord.ext.commands import Bot
-from discord.ext import commands
+# from discord.ext import commands
 import requests
 from bs4 import BeautifulSoup
-import datetime
+# import datetime
 import praw
 import discord
 import time
-import ast
-
+# import ast
 
 '''
 COMMANDS
@@ -32,24 +31,21 @@ COMMANDS
 -Embeds?? https://www.youtube.com/watch?v=XKQWxAaRgG0&t
 '''
 
-
-
-
-BOT_PREFIX = (".","")
-TOKEN = 'NDgyMTQ3NDM4MTE1Njg0MzYz.D3IkHg.pbRBXPTxphZUY1LwjA_gAp762qg'  # Get at discordapp.com/developers/applications/me
-role_id="461171949197066241"#Owner ROLE
+BOT_PREFIX = (".", "")
+# Get at discordapp.com/developers/applications/me
+TOKEN = 'NDgyMTQ3NDM4MTE1Njg0MzYz.D3IkHg.pbRBXPTxphZUY1LwjA_gAp762qg'
+role_id = "461171949197066241"  # Owner ROLE
 client = Bot(command_prefix=BOT_PREFIX)
 reddit = praw.Reddit(client_id='StI7zL-mxlm2HQ',
                      client_secret='lUkCF66UgqEJgE03JkkAkXWQzKA',
                      user_agent='PyBotNSFW')
 
-with open('Remembers.txt', 'r') as f:
-    Remembers = ast.literal_eval(f.read())
-##COMMANDS
-##=====================================================
+
+# COMMANDS
+# =====================================================
 
 
-##Eight Ball
+# Eight Ball
 @client.command(name='8ball',
                 description="Answers a yes/no question.",
                 brief="Answers from the beyond.",
@@ -66,66 +62,98 @@ async def eight_ball(context):
     await client.say(random.choice(possible_responses) + ", " + context.message.author.mention)
 
 
-
-##Urban Dictionary
+# Urban Dictionary
 @client.command(name='ud',
                 description="Gives the definition of the input word,searched in Urban Dictionary",
                 brief="ud <word>")
 async def ud(word):
     source = requests.get("http://www.urbandictionary.com/define.php?term={}".format(word))
     soup = BeautifulSoup(source.content, "html.parser")
-    msg= soup.find("div",attrs={"class":"meaning"}).text
-    await client.say (msg)
-##Wolfram
+    msg = soup.find("div", attrs={"class": "meaning"}).text
+    await client.say(msg)
+
+
+# Wolfram
 @client.command(name='wolfram',
                 description="Wolfram Alpha search-NO SPACE IN INPUT",
-                brief="wolfram <query(no spaces)>",pass_context=True)
-async def wolfram(self,*,ctx):
-    url = 'http://api.wolframalpha.com/v1/simple?appid=UQ2PAA-Y2QK6KVRA9&i={}'.format((ctx.replace(" ", "%20")).replace("+","%2B"))
+                brief="wolfram <query>", pass_context=True)
+async def wolfram(self,*, ctx):
+    url = 'http://api.wolframalpha.com/v1/simple?appid=UQ2PAA-Y2QK6KVRA9&i={}'.format(
+        (ctx.replace(" ", "%20")).replace("+", "%2B"))
 
-    await client.say (url)
+    await client.say(url)
 
 
-
-#NSFW
+# NSFW
 @client.command(name='nsfw',
                 description="Basically PORN",
                 brief="command: nsfw",
-                pass_context = True)
-async def nsfw(ctx,*,ctxnsfw:int):
-    nsfw = reddit.subreddit('nsfw+RealGirls+LegalTeens+Boobies+palegirls+redheads+ginger+Hotchickswithtattoos+bodyperfection+PrettyGirls+suicidegirls+goddesses+altgonewild+HighResNSFW+BonerMaterial+nsfw2+SexyFrex+boobs+lingerie+sexygirls+SnowWhites+iWantToFuckHer+SexyButNotPorn+fortyfivefiftyfive+braceface+JustHotWomen+thinspo+stripgirls+Page3Glamour+Playboy+GifsOfRemoval+wet+barelylegalteens+SceneGirls+NSFW_nospam+AlbumBabes+B_Cups+shewantstofuck+Barelylegal+girlsdoingnerdythings+classysexy+Sexy+NSFW_Wallpapers+PantyPeel+UHDnsfw+peachfuzz+UnrealGirls+FuckingPerfect+redhead+TheHottestBabes+slimgirls+primes+skivvies+THEGOLDSTANDARD+SoHotItHurts+nsfw_hd+18_20+18nsfw+Hotness+BareGirls+redlingerie+Perfect_NSFW+OnlyGoodPorn+fyeahsexyteens+AmazingTeens+fuckyeahsexyteens+HotGirls+paleskin+Babes+BetterThanPorn+nsfwnonporn+countrygirls+Playboy_Albums+realperfection+TeaGirls+ThinChicksWithTits+hq_nsfw+Straps+GorgeousGirlsNSFW+DomesticGirls+RealBeauties+Sexyness+StrippingOffShirts+RiaeSuicide+nsfw_bw+nsfw_sexy_girls+BacklitBeauty+SexyGoosebumps+tattooed_redheads+Bangable+TotalBabes+BikiniTeens+Headless+Randomgirls+HotGirlsNSFW+sexynsfw+Adultpics+debs_and_doxies+nsfwonly+nsfwnew').hot()
-    post_to_pick = random.randint(1, 15)
+                pass_context=True)
+async def nsfw(ctx, *, ctxnsfw: int):
+    # NSFW ROLE ID: 581924915507101701
+    server = ctx.message.server
+    author = ctx.message.author
+    role = discord.utils.get(server.roles, id="461179333407539200")
+    if(role in author.roles):
+        nsfw = reddit.subreddit(
+            'nsfw+RealGirls+LegalTeens+Boobies+palegirls+redheads+ginger+Hotchickswithtattoos+bodyperfection+PrettyGirls+suicidegirls+goddesses+altgonewild+HighResNSFW+BonerMaterial+nsfw2+SexyFrex+boobs+lingerie+sexygirls+SnowWhites+iWantToFuckHer+SexyButNotPorn+fortyfivefiftyfive+braceface+JustHotWomen+thinspo+stripgirls+Page3Glamour+Playboy+GifsOfRemoval+wet+barelylegalteens+SceneGirls+NSFW_nospam+AlbumBabes+B_Cups+shewantstofuck+Barelylegal+girlsdoingnerdythings+classysexy+Sexy+NSFW_Wallpapers+PantyPeel+UHDnsfw+peachfuzz+UnrealGirls+FuckingPerfect+redhead+TheHottestBabes+slimgirls+primes+skivvies+THEGOLDSTANDARD+SoHotItHurts+nsfw_hd+18_20+18nsfw+Hotness+BareGirls+redlingerie+Perfect_NSFW+OnlyGoodPorn+fyeahsexyteens+AmazingTeens+fuckyeahsexyteens+HotGirls+paleskin+Babes+BetterThanPorn+nsfwnonporn+countrygirls+Playboy_Albums+realperfection+TeaGirls+ThinChicksWithTits+hq_nsfw+Straps+GorgeousGirlsNSFW+DomesticGirls+RealBeauties+Sexyness+StrippingOffShirts+RiaeSuicide+nsfw_bw+nsfw_sexy_girls+BacklitBeauty+SexyGoosebumps+tattooed_redheads+Bangable+TotalBabes+BikiniTeens+Headless+Randomgirls+HotGirlsNSFW+sexynsfw+Adultpics+debs_and_doxies+nsfwonly+nsfwnew').hot()
+        post_to_pick = random.randint(1, 15)
+        amount = ctxnsfw
+        client.delete_message(ctx.message)
+        while amount != 0:
+            for i in range(0, post_to_pick):
+                submission = next(x for x in nsfw if not x.stickied)
 
-    amount=ctxnsfw
-    client.delete_message(ctx.message)
-    while amount!=0:
-        for i in range(0, post_to_pick):
-            submission = next(x for x in nsfw if not x.stickied)
+            await client.send_message(discord.Object(id="461240565980463128"), submission.url)
+            amount -= 1
+            time.sleep(2)
+        await client.delete_message(ctx.message)
+    else:
+        await client.say("You must be in nsfw role.\nTry the 'join nsfw' command")
 
-        await client.send_message (discord.Object(id="461240565980463128"),submission.url)
-        amount-=1
-        time.sleep(2)
-    await client.delete_message(ctx.message)
-
-
-
-
-#Clear messages
+# Clear messages
 @client.command(pass_context=True)
 async def clear(ctx, number):
-    server=ctx.message.server
-    author=ctx.message.author
-    role=discord.utils.get(server.roles,id=role_id)
+    server = ctx.message.server
+    author = ctx.message.author
+    role = discord.utils.get(server.roles, id=role_id)
     if role in author.roles:
-        number = int(number)+1 #Converting the amount of messages to delete to an integer
+        number = int(number) + 1  # Converting the amount of messages to delete to an integer
         counter = 0
-        async for x in client.logs_from(ctx.message.channel, limit = number):
+        async for x in client.logs_from(ctx.message.channel, limit=number):
             if counter < number:
                 await client.delete_message(x)
                 counter += 1
             await asyncio.sleep(0.2)
     else:
         await client.say("You are not permitted")
+
+
+
+#JOIN
+@client.command(pass_context=True)
+async def join(ctx,command:str):
+    joinables=["nsfw","nitain","catalyst","reactor"]
+    author = ctx.message.author
+    if command=="list":
+        res = ""
+        for i in joinables:
+            res = res + "\n" + str(i)
+        await client.say(res)
+    elif(command.lower() in joinables):
+        await client.add_roles(author, discord.utils.get(author.server.roles, name=command.lower()))
+#Leaves
+@client.command(pass_context=True)
+async def leave(ctx,command:str):
+    joinables=["nsfw","nitain","catalyst","reactor"]
+    author = ctx.message.author
+    if command=="list":
+        res = ""
+        for i in joinables:
+            res = res + "\n" + str(i)
+        await client.say(res)
+    elif(command.lower() in joinables):
+        await client.remove_roles(author, discord.utils.get(author.server.roles, name=command.lower()))
 
 
 
@@ -152,21 +180,17 @@ async def join(ctx, role: discord.Role = None):
             await client.remove_roles(ctx.message.author, role)
             return await client.say("{} role has been removed from {}."
                                       .format(role, ctx.message.author.mention))
+'''
 
-
-
-
-
-#AUTOROLE
+'''
+# AUTOROLE
 @client.event
 async def on_member_join(member):
     role = discord.utils.get(member.server.roles, name="Regular")
-    await client.add_roles(member,role)
-@client.event
-async def on_member_join(member):
-    msg= "Aqui va mensaje de bienvenida privado"
-    await client.send_message(member,)
-    '''
+    await client.add_roles(member, role)
+
+'''
+
 
 ##!PingPongs¡
 ##=====================================================
@@ -174,12 +198,11 @@ async def on_member_join(member):
 ##Hellos
 @client.command(pass_context=True)
 async def hello(context):
-    hellos = ["Hello","Wazzap!!!", "May the 4th be with You,","A bit late...","Goodnight!"]
+    hellos = ["Hello", "Wazzap!!!", "May the 4th be with You,", "A bit late...", "Goodnight!"]
     msg = random.choice(hellos) + "{}".format(context.message.author.mention)
     print(context.message.author.mention)
 
-    await client.say (msg)
-
+    await client.say(msg)
 
 
 ##Connection INFO
@@ -189,9 +212,11 @@ async def on_ready():
     await client.change_presence(game=Game(name="with Space-Time"))
     print("Logged in as " + client.user.name)
 
+@client.event
 async def on_member_join(member):
-    role=discord.utils.get(member.server.roles,name="Regurlar")
-    await client.add_roles(member,role)
+    await client.send_message(member, "TEST")
+    await client.add_roles(member, discord.utils.get(member.server.roles, name="Regular"))
+
 
 async def list_servers():
     await client.wait_until_ready()
@@ -200,10 +225,6 @@ async def list_servers():
         for server in client.servers:
             print(server.name)
         await asyncio.sleep(300)
-
-
-
-
 
 
 client.loop.create_task(list_servers())
